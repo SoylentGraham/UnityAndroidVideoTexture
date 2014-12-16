6f016dc9aee65fe519d565fe55deb2a9d199664e
Graham Reeves <graham@grahamreeves.com>
2014-12-16 13:10:33 +0000

    Tidied up code

--- Assets/MediaCodec.cs
+++ Assets/MediaCodec.cs
@@ -5,53 +5,67 @@ using System;
 
 //	http://bigflake.com/mediacodec/ExtractMpegFramesTest_egl14.java.txt
 
+
+class TMediaFormat
+{
+	public int		Width = 0;
+	public int		Height = 0;
+	public String	Mime = "";
+
+	public bool		IsValid()
+	{
+		return Width > 0 && Height > 0;
+	}
+};
+
 public class MediaCodec : MonoBehaviour {
 
 	public string mFilename = "stadium.ogg";
-	private string mLog = "Init\n";
+	private static string mLog = "Init\n";
 
-	// Use this for initialization
-	void Start () {
+	static void Log(String Message)	{
+			mLog += Message + "\n";
+	}
 
-		mLog += "Hello world \n";
-
+	static AndroidJavaObject LoadExtractor(String AssetFilename)
+	{
 		try
 		{
 			//	copy file
-#if UNITY_EDITOR
-			String StreamingFilename = "file:" + System.IO.Path.Combine(Application.streamingAssetsPath, mFilename);
-#else
-			String StreamingFilename = System.IO.Path.Combine(Application.streamingAssetsPath, mFilename);
-#endif
-			String PersistFilename = System.IO.Path.Combine(Application.persistentDataPath, mFilename);
-			mLog += "Loading www(" + StreamingFilename + ")\n";
+			#if UNITY_EDITOR
+			String StreamingFilename = "file:" + System.IO.Path.Combine(Application.streamingAssetsPath, AssetFilename);
+			#else
+			String StreamingFilename = System.IO.Path.Combine(Application.streamingAssetsPath, AssetFilename);
+			#endif
+			String PersistFilename = System.IO.Path.Combine(Application.persistentDataPath, AssetFilename);
+			Log("Loading www(" + StreamingFilename + ")");
 			WWW StreamingFile = new WWW(StreamingFilename);
 			while ( !StreamingFile.isDone )
 			{
-				mLog += "loading...." + StreamingFile.bytesDownloaded + "\n";
+				Log("loading...." + StreamingFile.bytesDownloaded );
 			}
 			if ( StreamingFile.error != null )
 			{
-				mLog += "www error: " + StreamingFile.error + "\n";
-				return;
+				Log ("www error: " + StreamingFile.error );
+				return null;
 			}
-			mLog += "Loaded: " +  StreamingFile.bytesDownloaded + "\n";
 
 			mLog += "Writing to persistent: " + PersistFilename + "\n";
 			System.IO.File.WriteAllBytes ( PersistFilename, StreamingFile.bytes );
-
-			mLog += "create extractor...\n";
+			
 			AndroidJavaObject extractor = new AndroidJavaObject("android.media.MediaExtractor");
-			mLog += "extractor: " + extractor.ToString() + "\n";
-			String Filename = PersistFilename;
-
-			if (!System.IO.File.Exists(Filename))
-				mLog += "File does not exist: " + Filename+"\n";
-			else {
-				var FileInfo = new System.IO.FileInfo(Filename);
-				mLog += "file exists: " + FileInfo.Length + "\n";
+			Log ("extractor: " + extractor.ToString() );
+			String LoadFilename = PersistFilename;
+			
+			if (!System.IO.File.Exists(LoadFilename))
+			{
+				Log ("File does not exist: " + LoadFilename );
+				return null;
 			}
 
+			var FileInfo = new System.IO.FileInfo(LoadFilename);
+			Log("file exists: " + FileInfo.Length);
+			
 			/*
 			//	make ram file
 			mLog += "making ram file \n";
@@ -62,35 +76,75 @@ public class MediaCodec : MonoBehaviour {
 			extractor.Call("setDataSource",RamFileDescriptor);
 			extractor.Call("setDataSource",Filename);
 */
+			
+			mLog += "setDataSource " + LoadFilename + "\n";
+			extractor.Call("setDataSource",LoadFilename);
 
-			mLog += "setDataSource " + Filename + "\n";
-			extractor.Call("setDataSource",Filename);
-
+			return extractor;
 			int TrackCount = extractor.Call<int>("getTrackCount");
 			mLog += "tracks: " + TrackCount + "\n";
 			int TrackIndex = 0;
 			mLog += "selectTrack " + TrackIndex + "\n";
 			extractor.Call ("selectTrack",TrackIndex);
-		
-			mLog += "getTrackFormat " + TrackIndex + "\n";
-			AndroidJavaObject format = extractor.Call<AndroidJavaObject>("getTrackFormat",TrackIndex);
-		
-			String FormatDescription = format.Call<String>("toString");
-			mLog += "format.toString() = " + FormatDescription + "\n";
+			return extractor;
+		}
+		catch ( Exception e )
+		{
+			Log ("Exception: " + e.Message );
+			return null;
+		}
+	}
 
+	static int GetTrack(AndroidJavaObject Extractor,String Filter="video/")
+	{
+		return 0;
+	}
+
+
+	static TMediaFormat GetFormat(AndroidJavaObject Extractor)
+	{
+		int TrackCount = Extractor.Call<int>("getTrackCount");
+		Log ("tracks: " + TrackCount);
+		int TrackIndex = GetTrack (Extractor);
+		Log ("selectTrack " + TrackIndex);
+		Extractor.Call ("selectTrack",TrackIndex);
+		
+		Log ("getTrackFormat " + TrackIndex);
+		try
+		{
+			AndroidJavaObject format = Extractor.Call<AndroidJavaObject>("getTrackFormat",TrackIndex);
+			String FormatDescription = format.Call<String>("toString");
+			Log("format.toString() = " + FormatDescription );
+			
 			//	sdk literals
 			const String KEY_MIME = "mime";
 			const String KEY_WIDTH = "width";
 			const String KEY_HEIGHT = "height";
-			String Mime = format.Call<string>("getString", KEY_MIME );
-			int Width = format.Call<int>("getInteger", KEY_WIDTH );
-			int Height = format.Call<int>("getInteger", KEY_HEIGHT );
-			mLog += "video=" + Mime + "; " + Width + "x" + Height + "\n";
+			TMediaFormat MediaFormat = new TMediaFormat();
+			MediaFormat.Mime = format.Call<string>("getString", KEY_MIME );
+			MediaFormat.Width = format.Call<int>("getInteger", KEY_WIDTH );
+			MediaFormat.Height = format.Call<int>("getInteger", KEY_HEIGHT );
+			return MediaFormat;
 		}
 		catch ( Exception e )
 		{
-			mLog += e.Message + "\n";
+			Log ("GetFormatException: " + e.Message );
+			return null;
 		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		AndroidJavaObject Extractor = LoadExtractor (mFilename);
+		if (Extractor == null)
+			return;
+
+		TMediaFormat Format = GetFormat( Extractor );
+		if (Format==null)
+			return;
+		Log ("Format of track is " + Format.Mime + " " + Format.Width + "x" + Format.Height);
 		//jo.Call<android.media.MediaCodec>("createDecoderByType");
 
 	}
