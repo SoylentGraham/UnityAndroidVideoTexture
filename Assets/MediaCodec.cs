a013eaf13c923deca6ae63ccba7100b1b1bfe6b2
Graham Reeves <graham@grahamreeves.com>
2014-12-18 18:34:19 +0000

    Seperated input & output into funcs, successfully reads through whole file

--- Assets/MediaCodec.cs
+++ Assets/MediaCodec.cs
@@ -24,16 +24,30 @@ class TMediaFormat
 public class MediaCodec : MonoBehaviour {
 
 	public string mFilename = "stadium.ogg";
-	private static string mLog = "Init\n";
+	private static List<string> mLog = new List<string>();
 
 	[NonSerialized]
 	private TVideoDecoder mDecoder;
+	[NonSerialized]
+	private AndroidJavaObject mExtractor;
+	private bool mFinishedExtracting = false;
+	private bool mFinishedDecoding = false;
 
-	static void Log(String Message)	{
-			mLog += Message + "\n";
+	const long TIMEOUT_USEC = 100;
+	const int INFO_TRY_AGAIN_LATER = -1;
+	const int INFO_OUTPUT_BUFFERS_CHANGED = -3;
+	const int INFO_OUTPUT_FORMAT_CHANGED = -2;
+	const int OTHER_ERROR = -99;
+	const int BUFFER_FLAG_END_OF_STREAM = 0x00000004;
+
+	public static void Log(String Message)
+	{
+		mLog.Add (Message);
+		while (mLog.Count > 30)
+			mLog.RemoveAt (0);
 	}
 
-	static AndroidJavaObject LoadExtractor(String AssetFilename)
+	private static AndroidJavaObject LoadExtractor(String AssetFilename)
 	{
 		try
 		{
@@ -48,7 +62,7 @@ public class MediaCodec : MonoBehaviour {
 			WWW StreamingFile = new WWW(StreamingFilename);
 			while ( !StreamingFile.isDone )
 			{
-				Log("loading...." + StreamingFile.bytesDownloaded );
+			//	Log("loading...." + StreamingFile.bytesDownloaded );
 			}
 			if ( StreamingFile.error != null )
 			{
@@ -56,11 +70,10 @@ public class MediaCodec : MonoBehaviour {
 				return null;
 			}
 
-			mLog += "Writing to persistent: " + PersistFilename + "\n";
+		//	Log("Writing to persistent: " + PersistFilename );
 			System.IO.File.WriteAllBytes ( PersistFilename, StreamingFile.bytes );
-			
-			AndroidJavaObject extractor = new AndroidJavaObject("android.media.MediaExtractor");
-			Log ("extractor: " + extractor.ToString() );
+
+			AndroidJavaObject Extractor = new AndroidJavaObject("android.media.MediaExtractor");
 			String LoadFilename = PersistFilename;
 			
 			if (!System.IO.File.Exists(LoadFilename))
@@ -70,7 +83,7 @@ public class MediaCodec : MonoBehaviour {
 			}
 
 			var FileInfo = new System.IO.FileInfo(LoadFilename);
-			Log("file exists: " + FileInfo.Length);
+			//Log("file exists: " + FileInfo.Length);
 			
 			/*
 			//	make ram file
@@ -83,15 +96,16 @@ public class MediaCodec : MonoBehaviour {
 			extractor.Call("setDataSource",Filename);
 */
 			
-			mLog += "setDataSource " + LoadFilename + "\n";
-			extractor.Call("setDataSource",LoadFilename);
+			Log ("setDataSource " + LoadFilename );
+			Extractor.Call("setDataSource",LoadFilename);
 
-			int TrackCount = extractor.Call<int>("getTrackCount");
-			mLog += "tracks: " + TrackCount + "\n";
-			int TrackIndex = 0;
-			mLog += "selectTrack " + TrackIndex + "\n";
-			extractor.Call ("selectTrack",TrackIndex);
-			return extractor;
+			int TrackCount = Extractor.Call<int>("getTrackCount");
+			if ( TrackCount < 1 )
+			{
+				Log("tracks: " + TrackCount);
+				return null;
+			}
+			return Extractor;
 		}
 		catch ( Exception e )
 		{
@@ -100,27 +114,27 @@ public class MediaCodec : MonoBehaviour {
 		}
 	}
 
-	static int GetTrack(AndroidJavaObject Extractor,String Filter="video/")
+	int GetTrack(AndroidJavaObject Extractor,String Filter="video/")
 	{
 		return 0;
 	}
 
 
-	static TMediaFormat GetFormat(AndroidJavaObject Extractor)
+	TMediaFormat GetFormat(AndroidJavaObject Extractor)
 	{
 		int TrackCount = Extractor.Call<int>("getTrackCount");
-		Log ("tracks: " + TrackCount);
+		//Log ("tracks: " + TrackCount);
 		int TrackIndex = GetTrack (Extractor);
-		Log ("selectTrack " + TrackIndex);
+		//Log ("selectTrack " + TrackIndex);
 		Extractor.Call ("selectTrack",TrackIndex);
 		
-		Log ("getTrackFormat " + TrackIndex);
+	//	Log ("getTrackFormat " + TrackIndex);
 		try
 		{
 			TMediaFormat MediaFormat = new TMediaFormat();
 			MediaFormat.Format = Extractor.Call<AndroidJavaObject>("getTrackFormat",TrackIndex);
 			String FormatDescription = MediaFormat.Format.Call<String>("toString");
-			Log("format.toString() = " + FormatDescription );
+		//	Log("format.toString() = " + FormatDescription );
 			
 			//	sdk literals
 			const String KEY_MIME = "mime";
@@ -160,7 +174,7 @@ public class MediaCodec : MonoBehaviour {
 
 		try
 		{
-			Log ("Creating codec for " + Format.Mime );
+		//	Log ("Creating codec for " + Format.Mime );
 			AndroidJavaClass MediaCodecClass = new AndroidJavaClass("android.media.MediaCodec");
 			Decoder.Decoder = MediaCodecClass.CallStatic<AndroidJavaObject>("createDecoderByType", Format.Mime );
 
@@ -177,111 +191,196 @@ public class MediaCodec : MonoBehaviour {
 		return Decoder;
 	}
 
+	int GetAndroidSDKLevel()
+	{
+		try
+		{
+			AndroidJavaClass VersionClass = new AndroidJavaClass ("android.os.Build$VERSION");
+			string Version = VersionClass.GetStatic<string> ("RELEASE");
+			int VersionInt = VersionClass.GetStatic<int> ("SDK_INT");
+			return VersionInt;
+		}
+		catch ( Exception e )
+		{
+			Log ("GetAndroidSDKLevel:: " + e.Message );
+			return -1;
+		}
+	}
+
+
 	// Use this for initialization
 	void Start () {
 
-		AndroidJavaObject Extractor = LoadExtractor (mFilename);
-		if (Extractor == null)
-			return;
+		int SdkLevel = GetAndroidSDKLevel ();
+		Log ("Android SDK " + SdkLevel);
 
-		TMediaFormat Format = GetFormat( Extractor );
+		mExtractor = LoadExtractor (mFilename);
+		if ( mExtractor == null )
+				return;
+
+		TMediaFormat Format = GetFormat( mExtractor );
 		if (Format==null)
 			return;
 		Log ("Format of track is " + Format.Mime + " " + Format.Width + "x" + Format.Height);
 		//jo.Call<android.media.MediaCodec>("createDecoderByType");
 
-		mDecoder = CreateDecoder (Extractor, Format);
+		mDecoder = CreateDecoder (mExtractor, Format);
 
-		try
-		{
-			DecodeFrame (Extractor);
-		}
-		catch ( Exception e )
-		{
-			Log ("decode frame Exception: " + e.Message);
-		}
+	
 	}
 
-	bool DecodeFrame(AndroidJavaObject Extractor)
+	AndroidJavaObject GetDecoderInputBuffer(AndroidJavaObject Decoder,int BufferIndex)
 	{
-		AndroidJavaObject Decoder = mDecoder.Decoder;
-		const int TIMEOUT_USEC = -1;
-		const int INFO_TRY_AGAIN_LATER = -1;
-		const int INFO_OUTPUT_BUFFERS_CHANGED = -3;
-		const int INFO_OUTPUT_FORMAT_CHANGED = -2;
-		const int OTHER_ERROR = -99;
-		const int BUFFER_FLAG_END_OF_STREAM = 0x00000004;
-		
-		//	get input buffer for codec
+		/*
+Truman:android-21 grahamr$ pwd
+/Users/grahamr/Library/Android/sdk/platforms/android-21
+Truman:android-21 grahamr$ javap -classpath android.jar -s android.media.MediaCodec
+*/
+		/*
+		string signature = "(I)Ljava/nio/ByteBuffer;";
+
+		IntPtr methodID = AndroidJNIHelper.GetMethodID( JavaObject.GetRawClass(), MethodName, signature, false );
+		Log ("Method ID (" + signature + ")== " + methodID);
+		return null;
+		string signature = "(i)ByteBuffer";
+		IntPtr methodID = AndroidJNIHelper.GetMethodID( JavaObject.m_jclass, MethodName, signature, isStatic );
+
+		jvalue[] array = AndroidJNIHelper.CreateJNIArgArray (args);
+		public static IntPtr GetMethodID (IntPtr javaClass, string methodName, string signature)
+		{
+			bool isStatic = false;
+			return AndroidJNIHelper.GetMethodID (javaClass, methodName, signature, isStatic);
+		}
+
+		if (typeof(ReturnType) == typeof(AndroidJavaObject))
+		{
+			IntPtr jobject = AndroidJNISafe.CallObjectMethod (this.m_jobject, methodID, array);
+			result = (ReturnType)((object)AndroidJavaObject.AndroidJavaObjectDeleteLocalRef (jobject));
+		}
+		else
+			
+			jvalue[] array = AndroidJNIHelper.CreateJNIArgArray (args);
+		ReturnType result;
+		*/
+
+		//	gr: API 19 "getInputBuffer(X) seems to be missing? checked running API and it's 19... hmmm
+		//	so use bufferS instead and access element
+		//AndroidJavaObject InputByteBuffer = Decoder.Call<AndroidJavaObject> ("getInputBuffer", InputBufferIndex);
+		AndroidJavaObject[] InputBuffers = Decoder.Call<AndroidJavaObject[]> ("getInputBuffers");
+		int BufferCount = InputBuffers.Length;
+		if (BufferIndex >= BufferCount)
+			return null;
+		return InputBuffers [BufferIndex];
+	}
+
+	bool UpdateCodecInput(AndroidJavaObject Decoder,AndroidJavaObject Extractor)
+	{
+		//	get input buffer to write into codec
 		int InputBufferIndex = Decoder.Call<int> ("dequeueInputBuffer", TIMEOUT_USEC);
-		Log ("dequeued decoder buffer " + InputBufferIndex);
 		if (InputBufferIndex < 0)
 			return false;
-		AndroidJavaObject InputByteBuffer = Decoder.Call<AndroidJavaObject> ("getInputBuffer", InputBufferIndex);
-
+		
+		Log ("Got input buffer buffer #" + InputBufferIndex);
+		AndroidJavaObject InputByteBuffer = GetDecoderInputBuffer (Decoder, InputBufferIndex);
+		if (InputByteBuffer == null) {
+			Log ("Error getting input buffer " + InputBufferIndex);
+			return false;
+		}
+		
 		//	pull latest data from extractor
 		int ChunkSize = Extractor.Call<int> ("readSampleData", InputByteBuffer, 0);
 		Log ("Extracted chunk size " + ChunkSize);
 		if (ChunkSize < 0)
 			ChunkSize = 0;
 		long presentationTimeUs = Extractor.Call<long> ("getSampleTime");
-		Log ("decoder presentation timed " + presentationTimeUs);
-
+		Log ("decoder presentation time " + presentationTimeUs);
+		
 		//	fill
 		//	send to decoder
 		int Flags = ChunkSize==0 ? BUFFER_FLAG_END_OF_STREAM : 0;
 		Decoder.Call("queueInputBuffer", InputBufferIndex, 0, ChunkSize, presentationTimeUs, Flags );
+		
+		bool AdvancedOk = Extractor.Call<bool>("advance");	//	gr: can we put this with read?
+		if (!AdvancedOk)
+			Log ("Extractor.advance() returned false");
 
-		Extractor.Call("advance");	//	gr: can we put this with read?
+		return true;
+	}
 
-		AndroidJavaObject BufferInfo = new AndroidJavaObject("android.media.MediaCodec.BufferInfo");
-		while ( true )
+	bool UpdateCodecOutput(AndroidJavaObject Decoder)
+	{
+		AndroidJavaObject BufferInfo = new AndroidJavaObject("android.media.MediaCodec$BufferInfo");
+
+		//	returns status if not buffer index
+		int OutputBufferIndex = Decoder.Call<int>("dequeueOutputBuffer", BufferInfo, TIMEOUT_USEC );
+			
+		bool Ready = true;
+		switch ( OutputBufferIndex )
 		{
-			//	returns status if not buffer index
-			int OutputBufferIndex = Decoder.Call<int>("dequeueOutputBuffer", BufferInfo, TIMEOUT_USEC );
-			Log ("decoder output buffer " + OutputBufferIndex);
-
-			bool Ready = true;
-			if ( OutputBufferIndex < 0 )
-				OutputBufferIndex = OTHER_ERROR;
-			switch ( OutputBufferIndex )
-			{
 			case INFO_TRY_AGAIN_LATER:
 			case INFO_OUTPUT_BUFFERS_CHANGED:
 			case INFO_OUTPUT_FORMAT_CHANGED:
 			case OTHER_ERROR:
 				Ready = false;
 				break;
-			}
-			if ( !Ready )
-				continue;
 
-			bool EndOfStream = (BufferInfo.Get<int>("flags") & BUFFER_FLAG_END_OF_STREAM) != 0;
-			if ( EndOfStream )
-			{
-				Log ("end of output stream");
-				break;
-			}
+			default:
+				if ( OutputBufferIndex < 0 )
+					Ready = false;
+			break;
+		}
+	
+		if ( !Ready )
+		{
+			//Log ("Not ready to output yet... " + OutputBufferIndex);
+			return false;
+		}
 
-			bool DoRender = ( BufferInfo.Get<int>("size") != 0 );
-
-			// As soon as we call releaseOutputBuffer, the buffer will be forwarded
-			// to SurfaceTexture to convert to a texture.  The API doesn't guarantee
-			// that the texture will be available before the call returns, so we
-			// need to wait for the onFrameAvailable callback to fire.
-			Decoder.Call("releaseOutputBuffer", OutputBufferIndex, DoRender);
+		Log ("got Output buffer #" + OutputBufferIndex);
+		bool EndOfStream = (BufferInfo.Get<int>("flags") & BUFFER_FLAG_END_OF_STREAM) != 0;
+		if ( EndOfStream )
+		{
+			Log ("end of output stream");
+			return false;
+		}
+			
+		bool DoRender = ( BufferInfo.Get<int>("size") != 0 );
+		Log ("Got buffer: render? " + DoRender);
+			
+		// As soon as we call releaseOutputBuffer, the buffer will be forwarded
+		// to SurfaceTexture to convert to a texture.  The API doesn't guarantee
+		// that the texture will be available before the call returns, so we
+		// need to wait for the onFrameAvailable callback to fire.
+		Decoder.Call("releaseOutputBuffer", OutputBufferIndex, DoRender);
 		//	outputSurface.awaitNewImage();
 		//	outputSurface.drawImage(true);
-	
-			break;
-		}		             
 
 		return true;
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
+
+		try
+		{
+			UpdateCodecInput ( mDecoder.Decoder, mExtractor );
+		}
+		catch ( Exception e )
+		{
+			Log ("UpdateCodecInput:: " + e.Message);
+		}
 		
+		
+		try
+		{
+			UpdateCodecOutput ( mDecoder.Decoder );
+		}
+		catch ( Exception e )
+		{
+			Log ("UpdateCodecOutput:: " + e.Message);
+		}
+
 	}
 	
 	void OnGUI()
@@ -291,7 +390,7 @@ public class MediaCodec : MonoBehaviour {
 		#if UNITY_EDITOR
 		Size = 20;
 		#endif
-		GUI.Label (new Rect (0, 0, Screen.width, Screen.height), "<size=" + Size + ">" + mLog + "</size>");
+		GUI.Label (new Rect (0, 0, Screen.width, Screen.height), "<size=" + Size + ">" + string.Join("\n",mLog.ToArray()) + "</size>");
 		
 		
 		if (mDecoder!=null && mDecoder.Texture) {
